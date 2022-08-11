704c4e6a18e7e43723dee3017a4aa0314fb90856
SarahYorke <108886868+SarahYorke@users.noreply.github.com>
2022-08-11 09:47:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function right1(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "green"
+}
+function wrong1(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "red"
+}
+function right2(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "green"
+}
+function wrong2(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "red"
+}
+function right3(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "green"
+}
+function wrong3(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "red"
+}
+function right4(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "green"
+}
+function wrong4(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "red"
+}
+function right5(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "green"
+}
+function wrong5(){
+    var a = document .getElementById("back")
+    a.style.backgroundColor = "red"
+}
